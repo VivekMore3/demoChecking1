b804e5a8c933bcdfb7a44ec00e859a776887bee0
vivekchopade662002 <vivek.chopade18@gmail.com>
2025-03-06 17:34:22 +0530

    intitial commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         int a = 10;
-        System.out.println("Hello Vivek1");
+        System.out.println("Hello Vivek1" +a);
         System.out.println(" practical");
         System.out.println("hello world");
 
