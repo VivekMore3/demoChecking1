e7b5fb251659df0fddf4753a3ac789e91e5d23a8
vivekchopade662002 <vivek.chopade18@gmail.com>
2025-03-06 13:52:58 +0530

    nsdi

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        //System.out.println("Hello, World!");
-       //done by vivek System.out.println("Hello Vivek1");
+        System.out.println("Hello, World!");
+        System.out.println("Hello Vivek1");
         System.out.println(" practical");
         System.out.println("hello world");
 
